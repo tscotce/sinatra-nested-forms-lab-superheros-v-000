20d6442effe0f99df9d34d5eaa04b8e47b2085aa
Tatyana Scotce <tscotce@gmail.com>
2018-09-14 21:11:09 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
+    
+    get '/' do
+      erb :super_hero
+    end
+    
+    post '/teams' do
+      # binding.pry
+      params[:user_input]
+
+      erb :team
+    end
 
 
 end
---
+++ app/models/team.rb
@@ -0,0 +1,16 @@
+# class Team
+#   attr_accessor :name, :motto
+  
+#   @@all = []
+  
+#   def initialize(name, motto)
+#     @name = name
+#     @motto = motto
+#     @@all << self
+#   end 
+  
+#   def self.all
+#     @@all
+#   end 
+  
+# end
